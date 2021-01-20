78563b7916c525b5d5d625408010c7e741a9b0fe
Heiko Beineke <heiko.beineke@gmail.com>
2021-01-20 18:34:51 +0100

    Display example phrases

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,22 @@
 <template>
   <Layout>
+    <div>
+      <div v-for="(edge, index) in $page.allPhrase.edges" :key="index">
+        <h1> {{edge.node.text}} </h1>
+      </div>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+      <div v-for="(edge, index) in $page.allTag.edges" :key="index">
+        <h1> {{edge.node.id}} </h1>
+      </div>
+    </div>
   </Layout>
 </template>
-
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Gridsome How to Blog"
   }
-}
+};
 </script>
 
 <style>
@@ -31,3 +24,24 @@ export default {
   margin-right: 1rem;
 }
 </style>
+
+<page-query>
+query {
+  allPhrase {
+    totalCount
+    edges {
+      node {
+        id
+        text
+      }
+    }
+  },
+  allTag {
+    edges {
+      node {
+        id
+      }
+    }
+  }
+}
+</page-query>
