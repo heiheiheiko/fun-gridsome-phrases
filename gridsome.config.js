7a4c22dd2416b1b69bca60567c98e4d2284ad14f
Heiko Beineke <heiko.beineke@gmail.com>
2021-01-20 18:34:42 +0100

    Parse example phrases

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,21 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: "Gridsome How to Blog",
+  siteDescription: "A how to blog designed with Gridsome",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "content/Phrase/**/*.md",
+        typeName: "Phrase",
+        refs: {
+          tags: {
+            typeName: "Tag",
+            create: true
+          }
+        }
+      }
+    },
+  ],
 }
