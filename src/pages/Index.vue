9cf47f51ca0e73c13e9d999b8602bc9a2460ec36
Heiko Beineke <heiko.beineke@gmail.com>
2021-01-20 23:48:06 +0100

    Remove copy alert

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -100,7 +100,6 @@ export default {
       copyText.select();
       copyText.setSelectionRange(0, 99999)
       document.execCommand("copy");
-      alert("Copied the text: " + copyText.value);
     }
   }
 };
