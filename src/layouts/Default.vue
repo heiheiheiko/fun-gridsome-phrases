4375af3af8d68913581ba10165216dceb2c27bd2
Heiko Beineke <heiko.beineke@gmail.com>
2021-01-20 23:44:33 +0100

    Add default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-container class="py-0 fill-height">
+        <v-toolbar-title>Phrase-o-mate</v-toolbar-title>
+      </v-container>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <slot></slot>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <static-query>
@@ -20,31 +21,3 @@ query {
   }
 }
 </static-query>
-
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
